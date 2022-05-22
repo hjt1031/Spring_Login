5be9a9c30a98414efd407bb52168828ea8886fdf
juntae ha <hjt1031@naver.com>
2022-05-22 19:56:37 +0900

    인터셉터 인증 체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,10 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
                 // 제외 시키는 부분
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
 
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "logout", "/css/**", "/*.ico", "/error");
     }
 
     //필터 부분
-    @Bean
+   // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
